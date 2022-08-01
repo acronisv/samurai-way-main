fddbc8711f8532cd01349038901d2945cd51fd6d
acronisv <acronisv@gmail.com>
2022-08-02 00:35:12 +0300

    Change app layout #11

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,38 @@
 import React from 'react';
 import './App.css';
-import {Technologies} from "./Technologies";
-import {Header} from "./Header";
-import {Footer} from "./Footer";
 
 const App = () => {
     return (
-        <div className="App">
-            <Header />
-            <Technologies />
-            <Footer/>
+        <div className="app_wrapper">
+            <header className="header">
+                Header
+            </header>
+            <nav className="nav">
+                <div>
+                    <a href="#">Profile</a>
+                </div>
+                <div>
+                    <a href="#">Messages</a>
+                </div>
+                <div>
+                    <a href="#">News</a>
+                </div>
+                <div>
+                    <a href="#">Music</a>
+                </div>
+                <div>
+                    <a href="#">Settings</a>
+                </div>
+            </nav>
+            <main className="main">
+                <div><img src="" alt="avatar"/></div>
+                <div>avatar desc</div>
+                <div>My posts</div>
+                <div>New posts</div>
+                <div>Posts 1</div>
+                <div>Posts 2</div>
+
+            </main>
         </div>
     );
 }
