4c7833f272d82382e1a029632e7b1b39d9301d93
acronisv <acronisv@gmail.com>
2022-08-03 01:31:40 +0300

    Divide layout into components #12

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,15 @@
 import React from 'react';
 import './App.css';
+import {Header} from "./components/Header";
+import {Navbar} from "./components/Navbar";
+import {Profile} from "./components/Profile";
 
 const App = () => {
     return (
         <div className="app_wrapper">
-            <header className="header">
-                Header
-            </header>
-            <nav className="nav">
-                <div>
-                    <a href="#">Profile</a>
-                </div>
-                <div>
-                    <a href="#">Messages</a>
-                </div>
-                <div>
-                    <a href="#">News</a>
-                </div>
-                <div>
-                    <a href="#">Music</a>
-                </div>
-                <div>
-                    <a href="#">Settings</a>
-                </div>
-            </nav>
-            <main className="main">
-                <div><img src="" alt="avatar"/></div>
-                <div>avatar desc</div>
-                <div>My posts</div>
-                <div>New posts</div>
-                <div>Posts 1</div>
-                <div>Posts 2</div>
-
-            </main>
+            <Header/>
+            <Navbar/>
+            <Profile/>
         </div>
     );
 }
---
+++ src/components/Profile.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const Profile = () => {
+    return (
+        <main className="main">
+            <div><img src="" alt="avatar"/></div>
+            <div>avatar desc</div>
+            <div>My posts</div>
+            <div>New posts</div>
+            <div>Posts 1</div>
+            <div>Posts 2</div>
+
+        </main>
+    )
+}
