be392059a375f4606df8d404471ac8032377c961
acronisv <acronisv@gmail.com>
2022-10-30 04:56:17 +0300

    Modify Dialogs page #21

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import s from './Dialogs.module.css'
 
 type DialogsPropsType = {
 
@@ -6,8 +7,26 @@ type DialogsPropsType = {
 
 export const Dialogs = (props: DialogsPropsType) => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialogsItem+ ' ' + s.dialogsItemActive}>
+                    Walter
+                </div>
+                <div className={s.dialogsItem}>
+                    John
+                </div>
+                <div className={s.dialogsItem}>
+                    Jorge
+                </div>
+                <div className={s.dialogsItem}>
+                    Pete
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hello</div>
+                <div className={s.message}>What's up?</div>
+                <div className={s.message}>Damn good</div>
+            </div>
         </div>
     );
 };
