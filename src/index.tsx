ca5c01f3f0126c9cf044515ccbd195e96a563909
acronisv <acronisv@gmail.com>
2022-11-21 01:20:02 +0300

    Fix rendering of components #47

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,15 @@
 import React from 'react';
 import './index.css';
-
 import ReactDOM from "react-dom";
 import App from "./App";
 import {store} from "./redux/redux-store";
 import {Provider} from "react-redux";
 
-const renderEntireTree = () => {
-    ReactDOM.render(
-        <Provider store={store}>
-            <App/>
-        </Provider>,
-        document.getElementById('root')
-    );
-}
+ReactDOM.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>,
+    document.getElementById('root')
+);
 
-renderEntireTree()
 
-store.subscribe(renderEntireTree)
--- src/redux/profile-reducer.ts
+++ src/redux/profile-reducer.ts
@@ -24,19 +24,23 @@ const initialState = {
 
 export const ProfileReducer = (state: ProfilePageStateType = initialState, action: ActionsType): ProfilePageStateType => {
     switch (action.type) {
-        case ADD_POST:
+        case ADD_POST: {
             const newPost: PostsType = {
                 id: new Date().getTime(),
                 message: action.postMessage,
                 likes: 0
             }
-            console.log(state)
-            state.posts.push(newPost)
-            state.newPostText = ''
-            return state
-        case UPDATE_NEW_POST_TEXT:
-            state.newPostText = action.newText
-            return state
+            let stateCopy = {...state}
+            stateCopy.posts = [...state.posts]
+            stateCopy.posts.push(newPost)
+            stateCopy.newPostText = ''
+            return stateCopy
+        }
+        case UPDATE_NEW_POST_TEXT: {
+            let stateCopy = {...state}
+            stateCopy.newPostText = action.newText
+            return stateCopy
+        }
         default:
             return state
     }
